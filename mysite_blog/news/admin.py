5b249856f8b0b02d8a0c9dceee83d4e57180a7be
EvgeniiyaR <tulpansik@mail.ru>
2022-07-13 23:23:19 +0300

    Add new model(Category), view(Category), templates(_nav, base, category)

--- mysite_blog/news/admin.py
+++ mysite_blog/news/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content', 'category')
+    list_editable = ('is_published', )
+    list_filter = ('is_published', 'category')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category')
+    list_display_links = ('id', 'category')
+    search_fields = ('category', )
+
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
--- mysite_blog/news/views.py
+++ mysite_blog/news/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 
 def index(request):
-    return HttpResponse('hello world')
+    news = News.objects.all()
+    categories = Category.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей',
+        'categories': categories,
+    }
+    return render(request=request, template_name='news/index.html', context=context)
 
 
-def test(request):
-    return HttpResponse('<h1>hello</h1>')
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'news': news,
+        'categories': categories,
+        'category': category,
+    }
+    return render(request=request, template_name='news/category.html', context=context)
